33f8184722885e0dd2434821289266da2416c360
Russnow <you@example.com>
2018-08-17 16:29:44 +0500

    led h0h1

--- workspace/Code/src/main.c
+++ workspace/Code/src/main.c
@@ -5,16 +5,19 @@
 int main (void)
 {
 	RCC->AHBENR|=RCC_AHBENR_GPIOBEN; //Enable tactirovanie porta B
-	
+	RCC->AHBENR|=RCC_AHBENR_GPIOHEN;
 	GPIOB->MODER=0x5000;//Ukazyvaem na PB6 & PB7 chto eto out
+	GPIOH->MODER=0x5;
 	
-	GPIOB->OTYPER=0; //podtyafivaem pins
+	GPIOB->OTYPER=0; //podhvatyvaem piny
+	GPIOH->OTYPER=0;
 	
 	GPIOB->OSPEEDR=0;
-	
+	GPIOH->OSPEEDR=0;
 	
 	while(1)
 	{
 		GPIOB->ODR=0xC0;
+		GPIOH->ODR=0x3;
 	}
 }

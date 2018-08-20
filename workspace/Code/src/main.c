aa99a73edd03b6e7ad08562e619d33d1c6e291ff
Russnow <you@example.com>
2018-08-20 14:12:50 +0500

    led B4 B5 B6 B7

--- workspace/Code/src/main.c
+++ workspace/Code/src/main.c
@@ -6,18 +6,19 @@ int main (void)
 {
 	RCC->AHBENR|=RCC_AHBENR_GPIOBEN; //Enable tactirovanie porta B
 	RCC->AHBENR|=RCC_AHBENR_GPIOHEN;
-	GPIOB->MODER=0x5000;//Ukazyvaem na PB6 & PB7 chto eto out
-	GPIOH->MODER=0x5;
+	GPIOB->MODER=0x5500;//Ukazyvaem na PB6 & PB7 chto eto out
+	//GPIOH->MODER=0x5;
 	
 	GPIOB->OTYPER=0; //podhvatyvaem piny
-	GPIOH->OTYPER=0;
+	//GPIOH->OTYPER=0;
 	
 	GPIOB->OSPEEDR=0;
-	GPIOH->OSPEEDR=0;
+	//GPIOH->OSPEEDR=0;
 	
 	while(1)
 	{
-		GPIOB->ODR=0xC0;
-		GPIOH->ODR=0x3;
+		
+		GPIOB->ODR=0xF0;
+		//GPIOH->ODR=0x3;
 	}
 }
